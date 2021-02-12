2573d1c01d747ea9e9068b63285978880b1f0992
Omar Palacios <omarpal88@gmail.com>
2021-02-11 21:02:53 -0500

    Pruebas de obtención de datos para realizar configuración del filtro

---
+++ js/index.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByClassName('card__keywords'));
+
+const keyWordsInCards = document.getElementById('job__card').getElementsByClassName('card__keywords');
+
+console.log(keyWordsInCards);

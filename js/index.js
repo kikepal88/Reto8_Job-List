8184e8aff60b3cce510f5d1b0f5ecdc2e6b7b68e
Omar Palacios <omarpal88@gmail.com>
2021-02-16 21:32:46 -0500

    Proyecto terminado, Se agregan algunos parametros a etiquetas html, se agregan algunos estilos, y se construye la logica para que funcione el filtro

--- js/index.js
+++ js/index.js
@@ -1,5 +1,57 @@
-console.log(document.getElementsByClassName('card__keywords'));
+const mainFilterContainer = document.getElementById('main__filter-container')
 
-const keyWordsInCards = document.getElementById('job__card').getElementsByClassName('card__keywords');
+const filters = Array.from(document.getElementsByClassName('filter'))
+const keywords = filters.map(val => {
+  return val.querySelector('.keyword')
+})
 
-console.log(keyWordsInCards);
+const jobCards = Array.from(document.getElementsByClassName('job__card'))
+
+let activeKeys = [];
+
+function keywordClicked() {
+  console.log(activeKeys)
+  const  attributeKey = event.currentTarget.getAttribute("data-key")
+  console.log(attributeKey)
+  keywords.map( (val, index) => {
+    const keywordVal = val.getAttribute("data-key")
+    attributeKey === keywordVal && filters[index].classList.toggle('show')
+    toggleElementInArray(activeKeys, attributeKey)
+  })
+  console.log(activeKeys)
+  activeKeys.length !== 0 ? mainFilterContainer.classList.add('show') : mainFilterContainer.classList.remove('show')
+  togglejobCard(activeKeys)
+}
+
+function clearFilter() {
+  activeKeys = []
+  togglejobCard(activeKeys)
+  filters.map( (val) => {
+    val.classList.remove('show')
+  })
+  mainFilterContainer.classList.remove('show')
+}
+
+function togglejobCard(keywords) {
+  const jobsCard = jobCards.map( (val, jobIndex) => {
+    const jobCard = Array.from(val.querySelectorAll('.card__keyword'))
+    const jobKeys = jobCard.map( card => {
+      return card.getAttribute("data-key")
+    })
+    let keywordsInProject = 0
+    validateKeywords = keywords.map( keyword => {
+      jobKeys.indexOf(keyword) > -1 && keywordsInProject++
+    })
+    console.log(keywordsInProject)
+    keywordsInProject === activeKeys.length ? val.classList.remove('hidden') : val.classList.add('hidden')
+  })
+}
+
+function toggleElementInArray(array, elem) {
+  const index = array.indexOf(elem);
+  if (index > -1) {
+      array.splice(index, 1);
+  } else {
+    array.push(elem)
+  }
+}

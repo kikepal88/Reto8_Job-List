c1f31d0528ae09eaa12c5974d450186b05113a0e
Omar Palacios <omarpal88@gmail.com>
2021-02-24 22:47:13 -0500

    Pruebas y analisis de Código JS para posible mejora

--- js/index.js
+++ js/index.js
@@ -1,24 +1,32 @@
+// mainFilterContainer trae el contenedor de los filtros
 const mainFilterContainer = document.getElementById('main__filter-container')
 
+// filters crear un array con todos las etiquetas que tengan el class 'filter', que son los articles de cada filtro en el contenedor de filtros.
 const filters = Array.from(document.getElementsByClassName('filter'))
+
+// crear un nuevo array con todos las etiquetas que tengan el class 'keyword', dentro de Filters, que son los div de cada filtro en el contenedor de filtros, estos tienen un data-key con el nombre de cada tecnología.
 const keywords = filters.map(val => {
   return val.querySelector('.keyword')
 })
 
+// crear un array con todos las etiquetas que tengan el class 'job_card', que son los articles de cada trabajo en el contenedor de cards.
 const jobCards = Array.from(document.getElementsByClassName('job__card'))
 
+// es un array vacío el cual va contener strings con los nombre de las tecnologías que entran al filtro.
 let activeKeys = [];
 
+// Esta es la función que se activa cuando se hace click en cada tecnologia de los cards o los filtros
 function keywordClicked() {
-  console.log(activeKeys)
   const  attributeKey = event.currentTarget.getAttribute("data-key")
-  console.log(attributeKey)
-  keywords.map( (val, index) => {
+  console.log('Tecnologia seleccionada', attributeKey)
+  const mapKeywords = keywords.map( (val, index) => {
     const keywordVal = val.getAttribute("data-key")
+    // Otra forma de hacer un if con &&
     attributeKey === keywordVal && filters[index].classList.toggle('show')
     toggleElementInArray(activeKeys, attributeKey)
   })
-  console.log(activeKeys)
+  console.log('mapKeywords', mapKeywords)
+  console.log('Tecnologias filtradas', activeKeys)
   activeKeys.length !== 0 ? mainFilterContainer.classList.add('show') : mainFilterContainer.classList.remove('show')
   togglejobCard(activeKeys)
 }
